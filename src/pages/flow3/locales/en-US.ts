1f0da260150ede53fbf051777e09d7e006ec2103
frog <wellfrog16@gmail.com>
2019-08-16 14:16:40 +0800

    first commit

---
+++ src/pages/flow3/locales/en-US.ts
@@ -0,0 +1,4 @@
+export default {
+  'flow3.description':
+    'The brain map is an effective graphical thinking tool for expressing divergent thinking. It is simple but effective and is a practical thinking tool',
+};
